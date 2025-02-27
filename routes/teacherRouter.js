5d7d4e9b19f703cebfa20d396cdb99dcfdb8beac
chrisvibeofficial <jueffizzy@gmail.com>
2025-02-27 21:22:57 +0100

    update teacher

--- routes/teacherRouter.js
+++ routes/teacherRouter.js
@@ -1,9 +1,10 @@
-const { verify, createScoreForStudent, updateStudent } = require('../controllers/teacherController');
+const { verify, createScoreForStudent, updateStudent, login } = require('../controllers/teacherController');
 const { authorizeTeacher } = require('../middlewares/authorization');
 const router = require('express').Router();
 
 
 router.get('/verify-account/:token', verify);
+router.post('/login', login)
 router.post('/create-score', authorizeTeacher, createScoreForStudent);
 router.put('/update-student/:studentId', authorizeTeacher, updateStudent);
 
